9b1b756a913578159a9bac4fdbb0b1a73fb57e09
Abogo <abogonono1@gmail.com>
2025-01-17 20:42:45 +0100

    a function named displayMessage that prints in STDOUT the string argument.

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+process.stdin.on('readable', () => {
+    const usrInput = process.stdin.read();
+
+    if (usrInput) {
+        process.stdout.write(`Your name is: ${usrInput}`);
+    }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});

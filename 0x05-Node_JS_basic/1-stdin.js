ccff12902782246117fdb07a26338f700be0618f
ABOGO <abogonono1@gmail.com>
2025-02-17 06:26:16 +0100

    indentation fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,13 +2,13 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('readable', () => {
-    const usrInput = process.stdin.read();
+  const usrInput = process.stdin.read();
 
-    if (usrInput) {
-        process.stdout.write(`Your name is: ${usrInput}`);
-    }
+  if (usrInput) {
+    process.stdout.write(`Your name is: ${usrInput}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
